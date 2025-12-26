6dcb876a4105ac21ee4aad3f7001784328a6dec9
Priyanhu <priyanshudhiman644@gmail.com>
2025-12-26 09:49:56 +0530

    add vercel.json file

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ import fetch from 'node-fetch'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: 'https://your-frontend.vercel.app'
+}));
 
 const PORT = process.env.PORT || 5000
 const api = process.env.API_KEY
