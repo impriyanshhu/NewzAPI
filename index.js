f13f8c8aa3e7d86f88bb2fa2cff3b7343afc665d
Priyanshu <163508521+impriyanshhu@users.noreply.github.com>
2025-12-26 22:30:16 +0530

    update cors

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
-app.use(cors());
+
+app.use(cors({
+  origin: "https://newzapi.onrender.com/",
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000
 const api = process.env.API_KEY
